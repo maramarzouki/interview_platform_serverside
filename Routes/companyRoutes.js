b2952cbaa102e9832da4907c70f542ba9c2903cd
maramarzouki <maramyoona123@gmail.com>
2023-03-29 09:57:15 +0100

    ~

--- Routes/companyRoutes.js
+++ Routes/companyRoutes.js
@@ -6,6 +6,6 @@ const router = Router ();
 router.get('/get_company_info/:recruiterID',company_controllers.get_company);
 router.get('/get_companies',company_controllers.get_all_companies);
 router.patch('/update_company/:recruiterID',company_controllers.update_company);
-router.delete('/delete_company',company_controllers.delete_company);
+// router.delete('/delete_company',company_controllers.delete_company);
  
 module.exports = router;
